dd53c7bff1a05223c3ea44e4ee7099af201ce03d
walterdyna <alexdyna@gmail.com>
2024-04-07 21:10:40 -0300

    Alterando elementos na prática

---
+++ scripts.js
@@ -0,0 +1,15 @@
+
+
+
+const element = document.querySelector("#main-input")
+
+
+element.placeholder = "Agora e esse texto"
+
+console.log(element.placeholder)
+
+
+
+const elementImg = document.querySelector("img")
+
+console.log(elementImg.src)
